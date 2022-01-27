064d4c77d2cd2e701b6c3715423e2c1959474e08
Aiden Townsend <aidenticklesgood@gmail.com>
2022-01-27 16:58:22 -0500

    Intake dashboard set.

---
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.intake;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Intake extends SubsystemBase {
+
+  private static Intake instance = null;
+  public static synchronized Intake getInstance() {
+    if (instance == null) instance = new Intake();
+    return instance;
+  }
+
+  TalonFX motor = new TalonFX(Constants.intake.MOTOR_ID);
+
+  /** Creates a new Intake. */
+  private Intake() {
+    motor.configFactoryDefault();
+    motor.configOpenloopRamp(Constants.intake.RAMP_RATE);
+
+    SmartDashboard.putNumber("Intake Voltage", 0.0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void set(double percent) {
+    motor.set(ControlMode.PercentOutput, percent);
+  }
+
+  public void setVoltage(double power) {
+    set(power / RobotController.getBatteryVoltage());
+  }
+}
